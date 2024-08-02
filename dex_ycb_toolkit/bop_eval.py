7cb3ebe46866b72553fafa9af9f964b6fc105955
Tella babatunde <htella@scu.edu>
2024-08-01 18:40:02 -0700

    Update

--- dex_ycb_toolkit/bop_eval.py
+++ dex_ycb_toolkit/bop_eval.py
@@ -17,6 +17,7 @@ from dex_ycb_toolkit.factory import get_dataset
 from dex_ycb_toolkit.logging import get_logger
 
 bop_toolkit_root = os.path.join(os.path.dirname(__file__), "..", "bop_toolkit")
+
 sys.path.append(bop_toolkit_root)
 
 from bop_toolkit_lib import dataset_params
